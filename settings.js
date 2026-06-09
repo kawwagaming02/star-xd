fabe5e29bafd3bdcd349cf834c9a1f696ce6c31a
kawwagaming02 <kawwagaming45@gmail.com>
2026-06-10 00:24:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@
 export default {
   // PUT YOUR SESSION ID BELOW (Required)
   // Get from: https://star.malvintech.sbs/
-  SESSION_ID: "",
+  SESSION_ID: "king~H4sIAAAAAAAAA5VU23KjOBT8F73GNTYXg3FVqhYwBozxJZgk9tY+yEhg7kQS5jLlf9/CmUzmYXc2y5OQVH369OnWd1CUMcUO7sD8O6hIfIUMD0vWVRjMgVaHISZgBBBkEMxBpImHY+welYu6LN+E67VckT3NRLTfNd75WfHCBEv+jvXF7BHcRqCqz1kc/Abw5Qxtx+LCfpFtdCk6RW+am4zzZSzLfrXYTNoGpWbg+P3z8RHcBkQYk7iIjOqCc0xg5uBuB2PyNfrHtVYZge5Wu9KJnwwtnrlJmxlWryjlynHapuU9KtlTiyu/Rn9h2meE/GV7iLK9HsDJagYtzrOzY3IONYf467KUluvtgtu/06dxVGBkI1ywmHVf1t1cmO14GR1fDB23y+UukVh1CsWzfZZdViiKYdIuOsi6nU2+RtzbPngu6uwaviUstWmTq82DtS1YusuQOw5XqjANvaK8mv6vxHfkwyvp/9J9G8nmmdtYLeFFb8dbD469Ma4pmz04kPS8y8RXrHmZJOy/Rl9f8WtDeVvL6do/i1MnPWO9TpBhOFrp7xZ8tw98o3njxTj9pA9ZTX7Hkogt2zftVnpOTeKFrMlnrTNr2VogqVZ5ghi9dGy58aytJcv7/QqPq4tmtWo/q+s87TW4F9TnKTdOmJR4O395LSgXRY/3jlLc2QjMudsIEBzFlBHI4rIY9nheGQGIrh4OCGZ3eQE5S2jS1idiv3Z6T0Vb79AMvY1Xl9fWovRZhpuT2aqnLts/ghGoSBlgSjGyYspK0rmYUhhhCuZ//jUCBW7Z++CGcgI3AmFMKPOLuspKiD6m+nEIg6CsC+Z1RaAPC0zAfPK5jRmLi4gOOtYFJMElvmL9AhkF8xBmFP/sEBOMwJyRGv9MrV6iQXjjOHE3quaBEcjvA4kRmANFlCVOVKYKL8356R/0WzOgwqr6VmAGRiC73+JFXhL4iTCRlOl0Jg83h4PbT4IDHsIMxhkdjLIVWPKwt4zVQS2RYZqqH6l6pILPhj6c8a68VW/TF7jZzY4Bz/X4eYX4kyX3TbLNDiFaHo4dvSQwu/IL9fEfQAazz/BkOl3uLkH+mtSa6C2laG0hmqrT1WniNd6Wmi87rnDHuhx1W9NYNFDvfQspSulzoSVYq2fX1T20InblrfpeNgJNfRyqIXyNA/xrMXsPD3UmudbaUKvttk5VU9fGpuGsRXR6XXWhbHvkgc+wt5xaVW2XTjR2zYme9y9t89rEjiowBV3yGlGHvCSHwHw7JOq7Z++ZyX68VfHdTcOoht8wxvfoF3AY4H+O7p33YLDJbfQLxI+35F/yqD3JSbXMk8PYtDYsEDLvFeVPD7OZ3Ve+IDz5vEvsI1+dlagBt9tfI1BlkIUlycEcwAKRMkZgBEhZD461i7D83YuiprbaROuh8QxSpn6m4BDnmDKYV2DOyTNuIkwUkR+BvFOrymOQfYQHqMPnhjtw+xuhKLqMWAcAAA==",
 
   // PUT YOUR REMOVE BACKGROUND KEY (Optional)
   // Get from: https://remove.bg/api
@@ -11,7 +11,7 @@ export default {
 
   // If you don't have session id, use pairing code
   // Put your WhatsApp number here (without +)
-  PAIRING_NUMBER: "",
+  PAIRING_NUMBER: "94761495926",
   
   SESSION_SITE: "https://pair.malvintech.sbs",
 
@@ -39,20 +39,20 @@ export default {
   PREFIXES: ".",
 
   // Bot Pack Name
-  PACKNAME: "sᴛᴀʀ-xᴅ",
+  PACKNAME: "kawwa-md",
 
   // Bot Name
-  BOT_NAME: "sᴛᴀʀ-xᴅ",
+  BOT_NAME: "kawwa-md",
 
   // Add Owner WhatsApp Number (without +)
-  OWNER_NUMBER: "263725787000",
+  OWNER_NUMBER: "94761495926",
 
   // Command Mode: "public" or "private"
   // private = only owner can use commands
   COMMAND_MODE: "private",
 
   // Owner Name
-  BOT_OWNER: "ᴍʀ xᴅᴋɪɴɢ",
+  BOT_OWNER: "ᴍʀ kawshala-md",
 
   // Timezone
   TIMEZONE: "Africa/Harare"
